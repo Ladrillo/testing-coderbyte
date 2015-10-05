8a66b0cff26ec001b4b32f7d87ef55d9cd676a78
Ladrillo <gabriel@cabrejas.net>
2015-10-04 23:23:17 -0600

    working on return longest string

--- test/test.js
+++ test/test.js
@@ -7,7 +7,6 @@ chai.should();
 
 
 
-
 // Using the JavaScript language, have the function firstReverse(str) take the str parameter being passed and return the string in reversed order.
 
 function firstReverse(str) {
@@ -58,19 +57,49 @@ function firstFactorial(num) {
 }
 
 describe('firstFactorial function', function () {
-    it('should return 1 if given 0', function (){
+    it('should return 1 if given 0', function () {
         expect(firstFactorial(0)).to.be.equal(1);
     });
 
-    it('should return 1 if given 1', function (){
+    it('should return 1 if given 1', function () {
         expect(firstFactorial(1)).to.be.equal(1);
     });
 
-    it('should return 2 if given 2', function (){
+    it('should return 2 if given 2', function () {
         expect(firstFactorial(2)).to.be.equal(2);
     });
 
     it('should return 120 if given 5', function () {
         expect(firstFactorial(5)).to.be.equal(120);
+    });
+});
+
+
+
+// Using the JavaScript language, have the function longestWord(sen) take the sen parameter being passed and return the largest word in the string. If there are two or more words that are the same length, return the first word from the string with that length. Ignore punctuation and assume sen will not be empty.
+
+function longestWord(sen) {
+    if (typeof sen !== 'string') throw 'error: incorrect type';
+    else if (sen === '') throw 'error: string empty';
+    else {
+        var splitSen = sen.split(/\W/);
+
+    }
+    return splitSen.join('');
+}
+
+describe('longestWord function', function () {
+    it('should return a string', function () {
+        expect(longestWord('something')).to.be.a('string');
+    });
+
+    it('should ignore punctuation', function () {
+        expect(longestWord('aaa.')).to.equal('aaa');
+    });
+
+    it('should return the longest word when all words unequal', function () {
+        expect(longestWord('a aa aaa')).to.equal('aaa');
+        expect(longestWord('aaa aa a')).to.equal('aaa');
+        expect(longestWord('a aaa aa')).to.equal('aaa');
     });
 });
